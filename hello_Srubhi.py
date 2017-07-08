bea21e50f77f1c5f467415dc9ce5f170fe6a931b
Saurav Ghosh <sauravgsh16@gmail.com>
2017-07-08 19:52:04 +0530

    Bootstarp first commit

--- hello_Srubhi.py
+++ hello_Srubhi.py
@@ -5,14 +5,23 @@ Created on Thu Jul 06 20:06:17 2017
 @author: user
 """
 
-from flask import Flask
+from flask import Flask, make_response, render_template
+from flask_bootstrap import Bootstrap
+#from flask.ext.script import Manager
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+#manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello Surbhi</h1>'
+    response = make_response('<h1>Hello Surbhi</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
     
 if __name__ == '__main__':
     app.run(debug=True)
-
